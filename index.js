bc7f3ce431d8a0a13b792050738452be22cd44f2
xb0y <sebasgt09@gmail.com>
2015-12-08 12:51:01 -0600

    lib path normalized

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var BaseQuery = require('./base.js');
 
 //Loading all queries available
-require("fs").readdirSync('./lib').forEach(function(file) {
+var normalizedPath = require("path").join(__dirname, "lib");
+require("fs").readdirSync(normalizedPath).forEach(function(file) {
     var queryName = file.replace('.js','');
     BaseQuery.queries[queryName] = require("./lib/" + file);
 });
